1a0e9d92c609507daba8fab54260bb5d27f64c31
Endah Nurjannah <jennahrosr@gmail.com>
2026-04-28 10:42:37 +0800

    Update database configuration

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $username = 'root';         // default XAMPP username
 $password = '';             // empty password for default XAMPP setup
 
 // PDO Configuration
-$dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+$dsn = "mysql:host=$host;port=3307;dbname=$dbname;charset=utf8mb4";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
